cef024f6e5a4bba9dbce414d048d6ad21f913c65
Techin Park <techin@MacBook-Pro-2.local>
2017-03-25 15:57:31 +0900

    First commit

---
+++ Bridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bridge
+//
+//  Created by Techin Park on 2017. 3. 25..
+//  Copyright © 2017년 tmsae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bridge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bridge
+//
+//  Created by Techin Park on 2017. 3. 25..
+//  Copyright © 2017년 tmsae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
